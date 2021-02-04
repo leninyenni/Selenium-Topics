9feeb2089658ecc4bdfe70de016263b99de6c349
Lenin Yenni <myseleniumwork@gmail.com>
2021-02-04 10:58:52 +0530

    First commit

---
+++ src/main/java/com/selenium/ZoomInZoomOutInSelenium.java
@@ -0,0 +1,31 @@
+package com.selenium;
+
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class ZoomInZoomOutInSelenium {
+
+	WebDriver driver;
+
+	@Test
+	public void zoomInZoomOutInSelenium() throws InterruptedException {
+		// First step set the driver location
+		// For Window user
+		// System.setProperty("webdriver.chrome.driver", "D://Drivers//chromedriver_win32//chromedriver.exe.exe");
+				
+		System.setProperty("webdriver.chrome.driver", "D://Drivers//chromedriver_win32//chromedriver.exe");
+		// create driver object	
+		driver = new ChromeDriver();
+		
+		driver.get("http://automationpractice.com/index.php");
+		// zoom out by 40%
+		((JavascriptExecutor) driver).executeScript("document.body.style.zoom='40%'");
+		
+		Thread.sleep(3000);
+		// zoom out by 100%
+		((JavascriptExecutor) driver).executeScript("document.body.style.zoom='100%'");
+		
+	}
+}
